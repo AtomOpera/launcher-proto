fa41165edb16b2e1aa44489d348a5196d4e416df
AtomOpera <64531968+AtomOpera@users.noreply.github.com>
2021-02-15 22:42:30 +0000

    build

---
+++ build/precache-manifest.cc2a39f9561f695d9bf4c38c33a21aaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dbddfb9e5f19adfe59e3dd4d50dd232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a6a825dd30592a22086",
+    "url": "/static/css/main.36cbff42.chunk.css"
+  },
+  {
+    "revision": "cfb1647f9d1790913de8",
+    "url": "/static/js/2.d8d910d4.chunk.js"
+  },
+  {
+    "revision": "829c96705fc3ce5f8f1ed66bf6148a78",
+    "url": "/static/js/2.d8d910d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3db73bc8beab224f17a2",
+    "url": "/static/js/3.5b6b3495.chunk.js"
+  },
+  {
+    "revision": "4a6a825dd30592a22086",
+    "url": "/static/js/main.d3f2e7b3.chunk.js"
+  },
+  {
+    "revision": "43ada120929a6d245e05",
+    "url": "/static/js/runtime-main.95d813dd.js"
+  }
+]);
